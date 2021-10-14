9c29671b7e57d165ce1723b90ffe6fd23b2d82d0
Alan Godwin <agodwin88@googlemail.com>
2021-10-14 13:26:06 +0100

    added if statement to prevent empty vaules being appended to list

--- Duplicate_files/find_duplicate_images.py
+++ Duplicate_files/find_duplicate_images.py
@@ -3,16 +3,19 @@ def image_list(image_list):
    content_list = image_list.readlines()
    return content_list
 
-total_mrc_files = image_list("total_mrc_files.txt")
+total_mrc_files = image_list("full_image_list.txt")
 done_tiffs = image_list("Converted_tiff_files.txt")
 unconverted_mrc_list = []
-unconverted = ""
+
 for files in total_mrc_files:
     for image in done_tiffs:
-        unconverted = ""
-        if files[:-5] == image[:-5]:
+        if files[24:-5] == image[:-5]:
             unconverted = ""
             break
         else:
             unconverted = files
-    unconverted_mrc_list.append(unconverted)
+    if unconverted != "":
+        unconverted_mrc_list.append(unconverted[:-1])
+
+print(unconverted_mrc_list)
+print(len(unconverted_mrc_list))
