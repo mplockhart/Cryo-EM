e70452778301c8d9bb64c48f7f057f80198d02c8
Alan Godwin <agodwin88@googlemail.com>
2021-10-14 12:06:04 +0100

    added for loop to compare lists

--- Duplicate_files/find_duplicate_images.py
+++ Duplicate_files/find_duplicate_images.py
@@ -3,4 +3,15 @@ def image_list(image_list):
    content_list = image_list.readlines()
    return content_list
 
-print(image_list("Converted_tiff_files.txt"))
+total_mrc_files = image_list("total_mrc_files.txt")
+done_tiffs = image_list("Converted_tiff_files.txt")
+unconverted_mrc_list = []
+unconverted = ""
+for files in total_mrc_files:
+    for image in done_tiffs:
+        unconverted = ""
+        if files[:-5] == image[:-5]:
+        unconverted = ""
+        else:
+            unconverted = files
+    unconverted_mrc_list.append(unconverted)
